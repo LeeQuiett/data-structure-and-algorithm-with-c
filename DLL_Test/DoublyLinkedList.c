c2310972e95a4db0bec46015cc2e75cbf104e08a
LeeQuiett <vkdnjpo1512@naver.com>
2024-08-07 09:33:58 +0900

    DLL 연산 중 생성/노드 소멸/리스트 소멸 작성

---
+++ DLL_Test/DoublyLinkedList.c
@@ -0,0 +1,42 @@
+#include "DoublyLinkedList.h"
+
+// 노드 생성
+Node* DLL_CreatNode(ElementType NewData)
+{
+	Node* NewNode = (Node*)malloc(sizeof(Node));
+
+	NewNode->data = NewData;
+	NewNode->NextNode = NULL;
+	NewNode->PrevNode = NULL;
+
+	return NewNode;
+}
+
+// 노드 소멸
+void DLL_DestroyNode(Node* Node)
+{
+	free(Node);
+}
+
+// 리스트 소멸
+int DLL_DestroyList(Node** Head)
+{
+	int count = 0;
+	Node* Current = *Head;
+	Node* Next;
+
+	if (*Head == NULL) {
+		puts("Error: Head is NULL!");
+		return;
+	}
+
+	while (Current != NULL)
+	{
+		Next = Current->NextNode;
+		DLL_DestroyNode(Current);
+		Current = Next;
+		count++;
+	}
+	*Head = NULL;
+	return count;
+}
