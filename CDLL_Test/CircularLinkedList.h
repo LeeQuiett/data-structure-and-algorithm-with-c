15f01414d429d207041e46456ea9bb8b49c8bc9b
LeeQuiett <vkdnjpo1512@naver.com>
2024-08-09 14:05:17 +0900

    CDLL 테스트 코드 작성

--- CDLL_Test/CircularLinkedList.h
+++ CDLL_Test/CircularLinkedList.h
@@ -10,3 +10,9 @@ typedef struct Node {
 
 void CDLL_AppendNode(Node** Head, Node* NewNode);
 void CDLL_RemoveNode(Node** Head, Node* Remove);
+//void CDLL_DestroyNode(Node* Node);
+void CDLL_DestroyList(Node* Head);
+Node* CDLL_CreatNode(int NewData);
+void CDLL_InsertAfter(Node* Current, Node* NewNode);
+Node* CDLL_GetNodeAt(Node* Head, int Location);
+int CDLL_GetNodeCount(Node* Head);
